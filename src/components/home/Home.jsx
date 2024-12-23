cd678bc4eae8792f3e02367a2d2dc506dd46402f
Lawar James Chaudhary <140185232+lawarjameschaudhary@users.noreply.github.com>
2024-12-23 16:15:11 +0545

    naya change

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,29 +1,8 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 
 const Home = () => {
-  const [menuVisible, setMenuVisible] = useState(false);
-  const menuRef = useRef(null);
-
-
-  const toggleMenu = () => {
-    setMenuVisible((prev) => !prev);
-  };
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (menuRef.current && !menuRef.current.contains(event.target)) {
-        setMenuVisible(false); // Close the menu
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
-
   return (
-    <div className='relative bg-[url(./src/assets/bg.jpg)] bg-cover bg-center text-white font-Fanta leading-9 pl-12 pr-12 pt-20 xl:pt-24 md:pt-40 border-b-[0.1px] border-blue-300 pb-20'>
+    <div className='bg-[url(./src/assets/bg.jpg)] bg-cover bg-center text-white font-Fanta leading-9  pl-12 pr-12 pt-20 xl:pt-24 md:pt-40 border-b-[0.1px] border-blue-300 pb-20'>
       <div className='flex flex-col items-center justify-center h-full gap-8 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl'>
         <h1 className='text-3xl sm:text-4xl md:text-5xl text-center font-bold'>
           Hello, I am{' '}
@@ -34,36 +13,37 @@ const Home = () => {
         <h6 className='text-lg sm:text-xl md:text-2xl text-center'>
           A Front End Developer | Graphics Designer
         </h6>
+        <button className='bg-transparent border border-blue-500 text-white p-3 px-8 shadow-md shadow-blue-500 rounded-full hover:shadow-lg hover:shadow-blue-500 hover:from-blue-400 hover:via-blue-800 hover:to-blue-950 hover:scale-105 duration-700 focus:outline-none hover:-translate-y-1'>
+  Hire me
+</button>
 
-        {/* Menu button */}
-        <button
-          onClick={toggleMenu}
-          className='bg-transparent border border-blue-500 text-white p-3 px-8 shadow-md shadow-blue-500 rounded-full hover:shadow-lg hover:shadow-blue-500 hover:from-blue-400 hover:via-blue-800 hover:to-blue-950 hover:scale-105 duration-700 focus:outline-none hover:-translate-y-1'
-        >
-          Menu
-        </button>
+        <div className='relative'>
+        <img src="./src/assets/image.png" alt="Developer-iamge" className='w-full max-w-[675px] mx-auto' />
 
-        {/* Sliding menu */}
-        <div
-          ref={menuRef}
-          className={`fixed top-0 right-0 h-full w-64 bg-white text-black shadow-lg transform ${
-            menuVisible ? 'translate-x-0' : 'translate-x-full'
-          } transition-transform duration-500 ease-in-out`}
-        >
-          <div className='flex flex-col p-6 h-full'>
-            <button
-              onClick={toggleMenu}
-              className='self-end text-gray-600 hover:text-gray-900'
-            >
-              ✕
-            </button>
-            <ul className='mt-4 space-y-4'>
-              <li className='hover:underline cursor-pointer'>Home</li>
-              <li className='hover:underline cursor-pointer'>About</li>
-              <li className='hover:underline cursor-pointer'>Projects</li>
-              <li className='hover:underline cursor-pointer'>Contact</li>
-            </ul>
-          </div>
+          <div className='md:border-2 p-1 md:p-4  rounded-full text-center text-black bg-white flex items-center gap-1 md:gap-5 absolute top-[20%] md:top-[15%] right-[5%] xl:-right-[5%]'>
+              <div className='Logo'><img src="./src/assets/tailwind.png" alt="" className='w-6 md:w-9' /></div>
+              <div className=' text-sm md:text-3xl'>Tailwind CSS</div>
+            </div>
+            <div className='md:border-2 p-1 md:p-4 rounded-full text-center bg-white flex items-center gap-1 md:gap-5 mr-20 absolute text-black top-[50%] sm:top-[45%] md:top-[45%] -left-[5%] md:-left-[15%] lg:-left-[20%] xl:-left-[35%]'>
+              <div className='Logo'><img src="./src/assets/photoshop.png" alt="" className='w-6 md:w-9'/></div>
+              <div className=' text-sm md:text-3xl'>Photoshop</div>
+            </div>
+            <div className='md:border-2 p-1 md:p-4 rounded-full  text-center text-black bg-white flex items-center gap-1 md:gap-5 px-6 absolute top-[75%] sm:top-[70%] md:top-[75%] md:-left-[5%] lg:-left-[15%] xl:-left-[25%]'>
+              <div className='Logo'><img src="./src/assets/Html.png" alt="" className='w-6 md:w-9'/></div>
+              <div className=' text-sm md:text-3xl'>Html </div>
+            </div>
+            <div className='border-2 p-1 md:p-4 rounded-full text-center text-black bg-white flex items-center gap-1 md:gap-5 absolute top-[25%] sm:top-[20%] md:top-[15%] sm:left-4 md:-left-0 xl:-left-[5%]'>
+              <div className='Logo'><img src="./src/assets/react.svg" alt="" className='w-6 md:w-9' /></div>
+              <div className=' text-sm md:text-3xl'>React JS</div>
+            </div>
+            <div className='border-2 p-1 md:p-4 rounded-full text-center bg-white flex text-black items-center gap-1 md:gap-5 absolute top-[45%] md:top-[45%] -right-[5%] md:-right-[10%] lg:-right-[20%] xl:-right-[35%]' >
+              <div className='Logo'><img src="./src/assets/javascript.png" alt="" className='w-6 md:w-9' /></div>
+              <div className=' text-sm md:text-3xl'>JavaScript</div>
+            </div>
+            <div className='border-2 p-1 md:p-4 rounded-full text-center text-black   bg-white flex items-center gap-1 md:gap-5 px-6 absolute top-[70%] md:top-[75%] right-[10%] md:-right-0 lg:-right-[16%]' >
+              <div className='Logo'><img src="./src/assets/css.png" alt="" className='w-6 md:w-9' /></div>
+              <div className=' text-sm md:text-3xl'>CSS</div>
+            </div>
         </div>
       </div>
     </div>
